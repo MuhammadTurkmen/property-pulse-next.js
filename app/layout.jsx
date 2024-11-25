d0e90f81fa06d090d8f4fa27b737fd62927f26e9
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-11-25 08:33:18 +0430

    adding navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,18 @@
+import Navbar from "@/components/Navbar";
+import "@/assets/styles/globals.css";
+
+export const metadata = {
+  title: "PropertyPulse | Find The Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
+};
+
 const MainLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div>{children}</div>
+        <Navbar />
+        <main>{children}</main>
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Link from "next/link";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <div>Home Page</div>
+    </div>
+  );
 };
 
 export default HomePage;
